30efe1c0f662990d23c8a1fa32d2f5982b9fdfd1
凌寒舞 <2273376391@qq.com>
2022-10-30 10:36:37 +0800

    启动类配置mapper扫描路径

--- src/main/java/com/hanwu/wiki/config/WikiApplication.java
+++ src/main/java/com/hanwu/wiki/config/WikiApplication.java
@@ -1,6 +1,8 @@
 package com.hanwu.wiki.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +10,7 @@ import org.springframework.core.env.Environment;
 @Slf4j
 @SpringBootApplication
 @ComponentScan("com.hanwu")
+@MapperScan("com.hanwu.wiki.mapper")
 public class  WikiApplication {
 
 	public static void main(String[] args) {
