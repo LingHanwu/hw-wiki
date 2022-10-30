18128787088b66e38839c5651fe5bc0677b03ebf
凌寒舞 <2273376391@qq.com>
2022-10-30 13:24:56 +0800

    制作CopyUtil封装 BeanUtils

--- src/main/java/com/hanwu/wiki/service/EbookService.java
+++ src/main/java/com/hanwu/wiki/service/EbookService.java
@@ -4,12 +4,11 @@ import com.hanwu.wiki.domain.EbookExample;
 import com.hanwu.wiki.mapper.EbookMapper;
 import com.hanwu.wiki.req.EbookReq;
 import com.hanwu.wiki.resp.EbookResp;
+import com.hanwu.wiki.utils.CopyUtil;
 import lombok.val;
-import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,25 +32,19 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
 //        查询结果
         val ebookList = ebookMapper.selectByExample(ebookExample);
-
-        val respList = new ArrayList<EbookResp>();
-
+//        val respList = new ArrayList<EbookResp>();
 //        for (Ebook ebook:ebookList) {
 //            val ebookResp = new EbookResp();
 ////            复制对象
 //            BeanUtils.copyProperties(ebook, ebookResp);
 //            respList.add(ebookResp);
 //        }
-        ebookList.forEach(ebook->{
-            val ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        });
+//        ebookList.forEach(ebook->{
+//            val ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        });
 
-        return respList;
-//        ebookList.forEach(ebook -> System.out.println(ebook));
-
-
+        return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 
 
