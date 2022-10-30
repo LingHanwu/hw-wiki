1882eeaed45184c21a76550e29b0ee1f489d6006
凌寒舞 <2273376391@qq.com>
2022-10-30 09:27:20 +0800

    测试接口，测试idea自带http调试工具，测试配置文件中定义参数

--- src/main/java/com/hanwu/wiki/controller/TestController.java
+++ src/main/java/com/hanwu/wiki/controller/TestController.java
@@ -1,9 +1,10 @@
 package com.hanwu.wiki.controller;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 /**
  * @ClassName TestController
  * @Description 测试使用
@@ -12,10 +13,40 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+
+
+//    读取配置文件参数
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "111";
+        return "111" + testHello;
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world ! post: " + name;
+    }
+
+    @GetMapping("/hello/{id}")
+    public String getId(@PathVariable Integer id){
+        return id + testHello;
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
