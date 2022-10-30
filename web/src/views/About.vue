d6a2daf9da80c4632c81792aad0870950e2ddfc4
凌寒舞 <2273376391@qq.com>
2022-10-31 00:33:00 +0800

    首页路由开发

--- web/src/views/About.vue
+++ web/src/views/About.vue
@@ -1,5 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h2>about</h2>
   </div>
 </template>
+
+<style lang="less" scoped>
+
+</style>
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h2>home</h2>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<style lang="less" scoped>
 
-export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
+</style>
