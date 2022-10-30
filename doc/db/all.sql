9610f8c87cb651e8def332abc4e0d12af6ae219d
凌寒舞 <2273376391@qq.com>
2022-10-30 11:13:20 +0800

    配置代码生成器mbg

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -5,7 +5,7 @@ create table `test`
 (
     `id`   bigint not null comment 'id',
     `name` varchar(50) comment '名称',
-    `password` varchar(50) comment '名称',
+    `password` varchar(50) comment '密码',
     primary key (`id`)
 ) engine = INNODB
   default charset = utf8mb4
@@ -13,4 +13,14 @@ create table `test`
 
 insert into `test`(id, name, password) value (1,'测试','123456');
 
-select * from `test`;
+drop table if exists `demo`;
+create table `demo`
+(
+    `id`   bigint not null comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = INNODB
+  default charset = utf8mb4
+    comment = '测试';
+
+insert into `demo`(id, name) value (1,'123456');
