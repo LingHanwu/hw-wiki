1bf531d2ee512c4132a8dbc07fb49295a68cfce0
凌寒舞 <2273376391@qq.com>
2022-10-30 08:50:04 +0800

    引入lombock，修改启动类配置日志输出，修改端口

--- src/main/java/com/hanwu/wiki/WikiApplication.java
+++ src/main/java/com/hanwu/wiki/WikiApplication.java
@@ -1,13 +1,23 @@
 package com.hanwu.wiki;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
+@Slf4j
 @SpringBootApplication
 public class  WikiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WikiApplication.class, args);
+		SpringApplication app = new SpringApplication(WikiApplication.class);
+		Environment env = app.run(args).getEnvironment();
+
+//		LOG.info("启动成功!");
+		log.info("项目启动成功！");
+		log.info("运行地址：\thttp://127.0.0.1:{}",env.getProperty("server.port"));
 	}
 
 }
