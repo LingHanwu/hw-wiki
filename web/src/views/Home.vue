1f914d380c400f440782a6af1e9c9054d27dbf19
凌寒舞 <2273376391@qq.com>
2022-11-01 17:04:52 +0800

    获取接口数据显示

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,17 +1,24 @@
 <script lang="ts" setup>
 import axios from 'axios'
-axios.get("http://127.0.0.1:8080/ebook/list?name=Python").then((response)=>{
-  console.log(response);
-  
+import { onMounted, ref } from 'vue';
+const ebooks = ref()
+// 生命周期
+onMounted(() => {
+  axios.get("http://127.0.0.1:8080/ebook/list?name=Python").then((response) => {
+    console.log(response);
+    const data = response.data;
+    ebooks.value = data.content;
+    console.log(ebooks);
+
+  })
 })
-
-
 
 </script>
 
 <template>
   <div class="home">
     <h2>home</h2>
+    <pre>{{ ebooks }}</pre>
   </div>
 </template>
 
